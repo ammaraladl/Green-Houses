dc11d02c647fd4b35a1d62facda3b32b334fcf68
Ammar El-Adl <75081982+ammaraladl@users.noreply.github.com>
2025-12-30 00:24:49 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links')
+    const icon = document.querySelector('.humburger-icon')
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+=======
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links')
+    const icon = document.querySelector('.humburger-icon')
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+>>>>>>> fd6a0949d139f8f9f5ec634fee5f25cc7836cbca
+}
